42ff598adf6799200ba65d9ee9be3f572a132706
PeaceF1 <77022147+PeaceF1@users.noreply.github.com>
2021-05-13 19:19:37 -0500

    commit

---
+++ src/myInfo.java
@@ -0,0 +1,10 @@
+public class myInfo {
+    public static void main(String[] args){
+        System.out.println("Peace Kanu-Asiegbu");
+        System.out.println("Java");
+        System.out.println("Omaha, NE");
+        System.out.println("Homemade Icecream");
+    }
+
+    
+}
